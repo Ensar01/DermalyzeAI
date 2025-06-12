2e47e97613d890425481955455ecff3ef246c21e
Ensar Mulahmetović <ensar.mulahmetovic@edu.fit.ba>
2025-06-12 23:29:39 +0200

    Kompletiran frontend + cleanup

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-import flask import Flask, render_template, request
+from flask import Flask, render_template, request
 import torch
 import torchvision.transforms as transforms
 
 from transformers import ViTForImageClassification, ViTImageProcessor
 from PIL import Image
 import os
+
+app= Flask(__name__)
 
 class_names = [
     'Acne and Rosacea Photos', 'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions', 'Atopic Dermatitis Photos', 
@@ -19,117 +21,117 @@ class_names = [
 
 class_details = {
     "Acne and Rosacea Photos": {
-        "Ime": "Akne i rozacea",
+        "naziv": "Akne i rozacea",
         "uzrok": "Poremećaji lojnih žlijezda, hormonalne promjene ili bakterije.",
         "mjera_predostroznosti": "Izbjegavati masnu kozmetiku, redovno čistiti lice, koristiti blage proizvode."
     },
     "Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions": {
-        "Ime": "Aktinična keratoza, bazocelularni karcinom i druge zloćudne lezije",
+        "naziv": "Aktinična keratoza, bazocelularni karcinom i druge zloćudne lezije",
         "uzrok": "Dugotrajno izlaganje UV zračenju.",
         "mjera_predostroznosti": "Koristiti zaštitu od sunca (kreme, odjeća), izbjegavati sunce u podne."
     },
     "Atopic Dermatitis Photos": {
-        "Ime": "Atopijski dermatitis",
+        "naziv": "Atopijski dermatitis",
         "uzrok": "Genetska predispozicija, suha koža i alergeni.",
         "mjera_predostroznosti": "Redovno hidratizirati kožu, izbjegavati poznate alergene i nadražaje."
     },
     "Bullous Disease Photos": {
-        "Ime": "Bulozne bolesti",
+        "naziv": "Bulozne bolesti",
         "uzrok": "Autoimune reakcije koje uzrokuju stvaranje mjehura na koži.",
         "mjera_predostroznosti": "Izbjegavati traumu kože, pratiti promjene i koristiti propisanu terapiju."
     },
     "Cellulitis Impetigo and other Bacterial Infections": {
-        "Ime": "Celulitis, impetigo i druge bakterijske infekcije",
+        "naziv": "Celulitis, impetigo i druge bakterijske infekcije",
         "uzrok": "Bakterije koje ulaze kroz oštećenu kožu.",
         "mjera_predostroznosti": "Održavati higijenu, čistiti rane, izbjegavati kontakt sa zaraženim osobama."
     },
     "Eczema Photos": {
-        "Ime": "Ekcem",
+        "naziv": "Ekcem",
         "uzrok": "Genetski faktori i osjetljivost kože na iritanse.",
         "mjera_predostroznosti": "Koristiti hidratantne kreme, izbjegavati iritanse i često tuširanje toplom vodom."
     },
     "Exanthems and Drug Eruptions": {
-        "Ime": "Egzantemi i reakcije na lijekove",
+        "naziv": "Egzantemi i reakcije na lijekove",
         "uzrok": "Alergijske reakcije na lijekove ili virusne infekcije.",
         "mjera_predostroznosti": "Izbjegavati poznate lijekove koji izazivaju reakciju, pratiti simptome i obratiti se ljekaru."
     },
     "Hair Loss Photos Alopecia and other Hair Diseases": {
-        "Ime": "Opadanje kose, alopecija i druge bolesti kose",
+        "naziv": "Opadanje kose, alopecija i druge bolesti kose",
         "uzrok": "Autoimune bolesti, stres, hormonski disbalans.",
         "mjera_predostroznosti": "Izbjegavati stres, hemijske tretmane i konzultovati dermatologa za terapiju."
     },
     "Herpes HPV and other STDs Photos": {
-        "Ime": "Herpes, HPV i druge polno prenosive bolesti",
+        "naziv": "Herpes, HPV i druge polno prenosive bolesti",
         "uzrok": "Virusne infekcije prenesene seksualnim kontaktom.",
         "mjera_predostroznosti": "Koristiti zaštitu tokom odnosa, redovno se testirati i izbjegavati rizične kontakte."
     },
     "Light Diseases and Disorders of Pigmentation": {
-        "Ime": "Bolesti izazvane svjetlošću i pigmentacijski poremećaji",
+        "naziv": "Bolesti izazvane svjetlošću i pigmentacijski poremećaji",
         "uzrok": "Poremećaji u proizvodnji melanina, UV zračenje ili genetski faktori.",
         "mjera_predostroznosti": "Izbjegavati direktno sunce, koristiti zaštitu od UV zračenja i dermatološke savjete."
     },
     "Lupus and other Connective Tissue diseases": {
-        "Ime": "Lupus i druge bolesti vezivnog tkiva",
+        "naziv": "Lupus i druge bolesti vezivnog tkiva",
         "uzrok": "Autoimune bolesti koje napadaju vezivno tkivo.",
         "mjera_predostroznosti": "Izbjegavati sunčevu svjetlost, pratiti simptome i redovno uzimati propisanu terapiju."
     },
     "Melanoma Skin Cancer Nevi and Moles": {
-        "Ime": "Melanom, rak kože, mladeži i pigmentne promjene",
+        "naziv": "Melanom, rak kože, mladeži i pigmentne promjene",
         "uzrok": "Maligne promjene ćelija kože uzrokovane UV zračenjem i genetikom.",
         "mjera_predostroznosti": "Koristiti zaštitne kreme, redovno pregledavati mladeže i posjećivati dermatologa."
     },
     "Nail Fungus and other Nail Disease": {
-        "Ime": "Gljivice na noktima i druge bolesti noktiju",
+        "naziv": "Gljivice na noktima i druge bolesti noktiju",
         "uzrok": "Gljivične infekcije, povrede noktiju ili loša higijena.",
         "mjera_predostroznosti": "Održavati higijenu noktiju, izbjegavati vlažna okruženja i koristiti antifungalne lijekove."
     },
     "Poison Ivy Photos and other Contact Dermatitis": {
-        "Ime": "Otrovni bršljan i drugi kontaktni dermatitis",
+        "naziv": "Otrovni bršljan i drugi kontaktni dermatitis",
         "uzrok": "Kontakt sa iritantima poput otrovnog bršljana ili hemikalija.",
         "mjera_predostroznosti": "Nositi zaštitnu odjeću, izbjegavati poznate iritanse i prati kožu nakon izlaganja."
     },
     "Psoriasis pictures Lichen Planus and related diseases": {
-        "Ime": "Psorijaza, lichen planus i srodne bolesti",
+        "naziv": "Psorijaza, lichen planus i srodne bolesti",
         "uzrok": "Autoimuni poremećaji koji ubrzavaju rast ćelija kože.",
         "mjera_predostroznosti": "Izbjegavati stres, koristiti hidratantne kreme i slijediti terapiju propisanu od strane ljekara."
     },
     "Scabies Lyme Disease and other Infestations and Bites": {
-        "Ime": "Šuga, lajmska bolest i druge infestacije i ujedi",
+        "naziv": "Šuga, lajmska bolest i druge infestacije i ujedi",
         "uzrok": "Paraziti, ugrizi insekata ili zaraženi krpelji.",
         "mjera_predostroznosti": "Održavati higijenu, izbjegavati kontakt sa zaraženima i koristiti zaštitu na otvorenom."
     },
     "Seborrheic Keratoses and other Benign Tumors": {
-        "Ime": "Seboroične keratoze i drugi benigni tumori",
+        "naziv": "Seboroične keratoze i drugi benigni tumori",
         "uzrok": "Benigne promjene na koži uzrokovane starenjem ili genetikom.",
         "mjera_predostroznosti": "Pratiti promjene na koži i savjetovati se sa dermatologom kod sumnjivih promjena."
     },
     "Systemic Disease": {
-        "Ime": "Sistemske bolesti",
+        "naziv": "Sistemske bolesti",
         "uzrok": "Unutrašnje bolesti koje se manifestuju na koži (npr. dijabetes, bolesti jetre).",
         "mjera_predostroznosti": "Kontrolisati osnovnu bolest, pratiti stanje kože i redovno se pregledati."
     },
     "Tinea Ringworm Candidiasis and other Fungal Infections": {
-        "Ime": "Tinea, gljivice, kandidijaza i druge gljivične infekcije",
+        "naziv": "Tinea, gljivice, kandidijaza i druge gljivične infekcije",
         "uzrok": "Gljivične infekcije kože ili sluznica.",
         "mjera_predostroznosti": "Održavati suhu i čistu kožu, izbjegavati dijeljenje ličnih stvari i koristiti protugljivične lijekove."
     },
     "Urticaria Hives": {
-        "Ime": "Urtikarija (koprivnjača)",
+        "naziv": "Urtikarija (koprivnjača)",
         "uzrok": "Alergijska reakcija na hranu, lijekove, stres ili druge podražaje.",
         "mjera_predostroznosti": "Izbjegavati poznate alergene, koristiti antihistaminike i smanjiti stres."
     },
     "Vascular Tumors": {
-        "Ime": "Vaskularni tumori",
+        "naziv": "Vaskularni tumori",
         "uzrok": "Abnormalan rast krvnih sudova, često benignog karaktera.",
         "mjera_predostroznosti": "Pratiti promjene, izbjegavati povrede i konsultovati se sa dermatologom za dalje korake."
     },
     "Vasculitis Photos": {
-        "Ime": "Vaskulitis",
+        "naziv": "Vaskulitis",
         "uzrok": "Upala krvnih sudova uzrokovana autoimunim ili infektivnim stanjima.",
         "mjera_predostroznosti": "Redovno pratiti stanje, izbjegavati infekcije i slijediti preporuke ljekara."
     },
     "Warts Molluscum and other Viral Infections": {
-        "Ime": "Bradavice, molluscum i druge virusne infekcije",
+        "naziv": "Bradavice, molluscum i druge virusne infekcije",
         "uzrok": "Virusne infekcije kože poput HPV-a ili Molluscum contagiosum.",
         "mjera_predostroznosti": "Izbjegavati direktan kontakt sa zaraženima, održavati higijenu i jačati imunitet."
     }
@@ -175,5 +177,35 @@ def classify_image(image_path):
     predicted_name = class_names[predicted_class.item()]
     confidence_score = confidence.item() * 100 
 
-    ime = class_details.get(predicted_name, {}).get("uzrok", "Nepoznato")
+    naziv = class_details.get(predicted_name, {}).get("naziv", "Nepoznato")
+    uzrok = class_details.get(predicted_name, {}).get("uzrok", "Nepoznato")
     mjera_predostroznosti = class_details.get(predicted_name, {}).get("mjera_predostroznosti", "Nepoznato")
+
+    return naziv, confidence_score, uzrok, mjera_predostroznosti
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    if request.method=="POST":
+        if 'file' not in request.files:
+            return "No file part"
+
+        file = request.files['file']
+        
+        if file.filename == "":
+            return "No selected file"
+        
+        file_path = os.path.join("static", file.filename)
+        file.save(file_path)
+  
+
+        naziv, confidence_score, uzrok, mjera_predostroznosti = classify_image(file_path)
+
+        return render_template(
+            "Predict.html", uploaded_image = file_path,
+            naziv= naziv, confidence_score = confidence_score,
+            uzrok = uzrok, mjera_predostroznosti = mjera_predostroznosti
+        )
+    return render_template("Predict.html")
+
+if __name__ =="__main__":
+    app.run(debug=True)
